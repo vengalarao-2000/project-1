15fc5df1d6aee090fd9cdc99b24230466f612d4e
Vengala Rao <vengalraopenubakula2000@gmail.com>
2025-10-06 10:54:13 -0700

    signin navbar fixed

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,22 +3,21 @@ export function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm">
             <div className="container">
-                <a class="navbar-brand d-flex align-items-center fw-bold" href="home.html">
+                <a className="navbar-brand d-flex align-items-center fw-bold" href="/home.html">
                     <img src="/assets/images/logo_pap.png" alt="Paws & Pixels logo" height="50" width="50"
-                        class="d-inline-block align-text-top" />
+                        className="d-inline-block align-text-top" />
                     <span>Paws & Pixels</span>
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav"
-                    aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#nav" aria-controls="nav" aria-expanded="false"
+                    aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="nav">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item"><a className="nav-link" href="/home.html">Home</a></li>
                         <li className="nav-item"><a className="nav-link" href="/about.html">About</a></li>
                         <li className="nav-item"><a className="nav-link" href="/contact.html">Contact Us</a></li>
-                        <li className="nav-item"><Link className="nav-link" to="/signin">Sign in</Link></li>
-                    </ul>
+                        <li className="nav-item"><Link className="nav-link" to="/signin">Sign in</Link></li> </ul>
                 </div>
             </div>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "./NavBar.jsx";
 import SignInApp from "./SignInApp.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
